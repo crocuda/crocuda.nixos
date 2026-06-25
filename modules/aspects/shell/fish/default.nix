bb788fa890b913cacc5b7ceb41f84e9d0379d792
areskill <areskul@areskul.com>
2026-06-25 13:26:10 +0200

    fix: bad closing bracket.

--- modules/aspects/shell/fish/default.nix
+++ modules/aspects/shell/fish/default.nix
@@ -78,100 +78,100 @@
         };
       };
     };
-    crocuda.shell.utils = {
-      homeManager = {pkgs, ...}: {
-        home.files = {
-          # Nushell
-          ".config/nushell/config.nu".source = dotfiles/nushell/config.nu;
-          ".config/nushell/env.nu".source = dotfiles/nushell/env.nu;
+  };
+  crocuda.shell.utils = {
+    homeManager = {pkgs, ...}: {
+      home.files = {
+        # Nushell
+        ".config/nushell/config.nu".source = dotfiles/nushell/config.nu;
+        ".config/nushell/env.nu".source = dotfiles/nushell/env.nu;
 
-          # Process management
-          # ".config/htop/htoprc".source = dotfiles/htop/htoprc;
-          # Atuin
-          ".config/atuin".source = dotfiles/atuin;
+        # Process management
+        # ".config/htop/htoprc".source = dotfiles/htop/htoprc;
+        # Atuin
+        ".config/atuin".source = dotfiles/atuin;
 
-          ## Key bindings for colemak-DH
-          ".config/fish/conf.d/interactive.fish".source = dotfiles/fish/interactive.fish;
-          ".config/fish/conf.d/colemak.fish".source = dotfiles/fish/colemak.fish;
+        ## Key bindings for colemak-DH
+        ".config/fish/conf.d/interactive.fish".source = dotfiles/fish/interactive.fish;
+        ".config/fish/conf.d/colemak.fish".source = dotfiles/fish/colemak.fish;
 
-          # siketyan/ghr plugin and completion
-          ".config/fish/conf.d/ghr.fish".text =
-            ''
-              ghr shell fish | source
-              ghr shell fish --completion | source
-            ''
-            # Custom functions for fast source code browsing
-            + ''
-              function gcd;
-                set dest $(ghr search "$argv" | head -n 1)
-                ghr cd $dest
-              end
-              function gnv;
-                set dest $(ghr search "$argv" | head -n 1)
-                ghr cd $dest && nvid && exit
-              end
-            '';
-          ".ghr/ghr.toml".text = inputs.nix-std.lib.serde.toTOML {
-            applications.nvid = {
-              cmd = "${pkgs.neovide}/bin/neovide";
-              args = ["%p"];
-            };
+        # siketyan/ghr plugin and completion
+        ".config/fish/conf.d/ghr.fish".text =
+          ''
+            ghr shell fish | source
+            ghr shell fish --completion | source
+          ''
+          # Custom functions for fast source code browsing
+          + ''
+            function gcd;
+              set dest $(ghr search "$argv" | head -n 1)
+              ghr cd $dest
+            end
+            function gnv;
+              set dest $(ghr search "$argv" | head -n 1)
+              ghr cd $dest && nvid && exit
+            end
+          '';
+        ".ghr/ghr.toml".text = inputs.nix-std.lib.serde.toTOML {
+          applications.nvid = {
+            cmd = "${pkgs.neovide}/bin/neovide";
+            args = ["%p"];
           };
         };
-        programs = {
-          direnv = {
-            enable = true;
-            nix-direnv.enable = true;
-          };
-          atuin = {
-            enable = true;
-            enableFishIntegration = true;
-          };
-          skim = {
-            enable = true;
-            enableFishIntegration = true;
-          };
-          pay-respects = {
-            enable = false;
-          };
-        };
-        home.packages = with pkgs; [
-          # File convertion
-          # dasel
-
-          # Get info on dir
-          fastfetch
-          onefetch
-
-          # Js utils
-          # jo
-          jq
-          # yq-go
-
-          # Inspect fs and io
-
-          # Disk
-          duf # df replacement (go)
-          dysk # df replacement (rust)
-          dua
-
-          pciutils
-          lshw
-
-          # Process management
-          # btop
-          lsof
-
-          # Linux capabilities
-          libcap
-
-          # ssh
-          ggh
-
-          # Git repository manager
-          siketyan-ghr
-        ];
       };
+      programs = {
+        direnv = {
+          enable = true;
+          nix-direnv.enable = true;
+        };
+        atuin = {
+          enable = true;
+          enableFishIntegration = true;
+        };
+        skim = {
+          enable = true;
+          enableFishIntegration = true;
+        };
+        pay-respects = {
+          enable = false;
+        };
+      };
+      home.packages = with pkgs; [
+        # File convertion
+        # dasel
+
+        # Get info on dir
+        fastfetch
+        onefetch
+
+        # Js utils
+        # jo
+        jq
+        # yq-go
+
+        # Inspect fs and io
+
+        # Disk
+        duf # df replacement (go)
+        dysk # df replacement (rust)
+        dua
+
+        pciutils
+        lshw
+
+        # Process management
+        # btop
+        lsof
+
+        # Linux capabilities
+        libcap
+
+        # ssh
+        ggh
+
+        # Git repository manager
+        siketyan-ghr
+      ];
     };
   };
 }
