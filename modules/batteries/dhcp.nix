f82511f16b2faa2c2ac539e61720f142594b801a
areskill <areskul@areskul.com>
2026-08-24 11:03:48 +0200

    batteries: stable dhcp-duuid

--- modules/batteries/dhcp.nix
+++ modules/batteries/dhcp.nix
@@ -1,16 +1,14 @@
 {inputs, ...}: let
   crocuda_lib = inputs.crocuda.crocuda_lib;
 in {
-  crocuda.batteries.dhcp_stable_duid-uuid = secret: {
+  crocuda.batteries.dhcp_stable_duid-uuid = {
     nixos = {config, ...}: {
       systemd.tmpfiles.rules = let
         user = "dhcpcd";
         group = "wheel"; # wheel | users
-        content = ''
-          ${crocuda_lib.network.str_to_duid-uuid config.networking.hostName}
-        '';
+        content = "${crocuda_lib.network.str_to_duid-uuid config.networking.hostName}";
       in [
-        "f '/var/lib/dhcpcd/duid' 664 ${user} ${group} - ${content}"
+        "f+ '/var/lib/dhcpcd/duid' 664 ${user} ${group} - ${content}"
       ];
     };
   };
